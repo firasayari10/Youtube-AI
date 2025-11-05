e639d043da4f6485f4d1308fcbc81eb95ee3e3fd
firasayari10 <ayari.firas@esprit.tn>
2025-11-05 23:52:40 +0100

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import { Button} from "@/components/ui/button"
+export default function Home() {
+  return (
+   <div>
+    landing page
+    <Button variant="new">
+      button
+    </Button>
+   </div>
+  );
+}
