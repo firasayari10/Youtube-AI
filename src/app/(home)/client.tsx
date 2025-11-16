828589e9e5882ec99ca8499f2ebe39147c1a5d5c
firasayari10 <ayari.firas@esprit.tn>
2025-11-16 23:34:41 +0100

    trpc routers organizattion with categories

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -2,26 +2,12 @@
 import { trpc} from "@/trpc/client";
 
 export const PageClient = ()=>{
-    const { data, error, isLoading } = trpc.hello.useQuery({
-        text:"firas"
-    })
+   const [data] = trpc.categories.getMany.useSuspenseQuery();
+
+   return (
+    <div>
+        {JSON.stringify(data)}
+    </div>
+   )
     
-    if (error) {
-        return (
-            <div className="p-4 bg-red-50 border border-red-200 rounded">
-                <p className="font-bold text-red-700">Error:</p>
-                <p className="text-red-600">{error.message}</p>
-            </div>
-        )
-    }
-    
-    if (isLoading) {
-        return <div>Loading...</div>
-    }
-    
-    return (
-        <div>
-            Page Client says: {data?.greeting}
-        </div>
-    )
 }
