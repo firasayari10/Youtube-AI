69e014de065b7562d3894e0f0a16b717da094ee0
firasayari10 <ayari.firas@esprit.tn>
2025-11-19 22:32:16 +0100

    studi layout

---
+++ src/app/(studio)/layout.tsx
@@ -0,0 +1,17 @@
+// app/(home)/layout.tsx
+import { StudioLayout } from "@/modules/studio/ui/layouts/studio-layout";
+import { TRPCProvider } from "@/trpc/client";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <StudioLayout>
+      <TRPCProvider>
+      {children}
+      </TRPCProvider>
+    </StudioLayout>
+  );
+}
---
+++ src/app/(studio)/studio/page.tsx
@@ -0,0 +1,10 @@
+const Page =( )=>{
+    return(
+        <div>
+            Studio
+        </div>
+        
+    )
+}
+
+export default Page;
