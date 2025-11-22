0d7dc50b87dbd94f6dbaef7c0d65ed2696426b9b
firasayari10 <ayari.firas@esprit.tn>
2025-11-22 23:59:37 +0100

    uplaoder component

--- src/modules/studio/ui/layouts/components/studio-uploader.tsx
+++ src/modules/studio/ui/layouts/components/studio-uploader.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
 import MuxUploader,{
     MuxUploaderDrop,MuxUploaderStatus,MuxUploaderFileSelect,MuxUploaderProgress
 } from "@mux/mux-uploader-react";
+import { UploadIcon } from "lucide-react";
 
 
 interface StudioUploaderProps{
@@ -8,11 +10,44 @@ interface StudioUploaderProps{
     onSuccess:()=>void;
 };
 
-export const StudioUploader = ({endpoint, onSuccess}:
+const UPLOADER_ID="video-uploader";
+
+export const StudioUploader = ({endpoint, }:
     StudioUploaderProps)=>{
         return(
             <div>
-                <MuxUploader endpoint={endpoint} />
+                <MuxUploader endpoint={endpoint}
+                id={UPLOADER_ID}
+                className="hidden group/uploader" />
+                <MuxUploaderDrop muxUploader={UPLOADER_ID} className="group/drop">
+                <div slot="heading" className="flex flex-col items-center gap-6">
+                    <div className="flex items-center justify-center gap-2 rounded-full bg-muted h-32 w-32">
+                        <UploadIcon  className="size-10 text-muted-foreground group/drop-[&[active]]:animate-bounce transition-all duration-300"/>
+                    </div>
+                    <div className="flex flex-col gap-2 text-center">
+                        <p className="text-sm">
+                            Drag and Drop video files you want to upload Here
+
+                        </p>
+                        <p className="text-xs text-muted-foreground">
+                            your videos will be private until you publish them
+                        </p>
+
+                    </div>
+                    <MuxUploaderFileSelect muxUploader={UPLOADER_ID}>
+                        <Button type="button" className="rounded-full">
+                            Select Files 
+
+                        </Button>
+
+                    </MuxUploaderFileSelect>
+
+                </div>
+                <span slot="seperator" className="hidden"/>
+                <MuxUploaderStatus muxUploader={UPLOADER_ID} className="text-sm"/>
+                <MuxUploaderProgress muxUploader={UPLOADER_ID} className="text-sm" type="percentage"/>
+                <MuxUploaderProgress muxUploader={UPLOADER_ID} type="bar"/>
+                </MuxUploaderDrop>
             </div>
         )
 
