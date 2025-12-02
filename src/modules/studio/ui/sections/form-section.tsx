26e874d3740bf4cd85b633372f616f620f247124
firasayari10 <ayari.firas@esprit.tn>
2025-12-02 23:54:51 +0100

    video loading in main page

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -58,70 +58,65 @@ export const FormSection = ({ videoId }: FormSectionProps) => {
 }
 
 const FallbackSkeleton = () => {
-    return (
-        <div>
-            <div className="flex items-center justify-between mb-6">
-                <div className="space-y-2">
-                    <Skeleton className="h-7 w-32"/>
-                    <Skeleton className="h-4 w-40"/>
-
-                </div>
-            <Skeleton className="h-9 w-24"/>
-            </div>
-            <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
-                <div className="space-y-8 lg:col-span-3">
-                         <div className="space-y-2">
-                    <Skeleton className="h-5 w-16"/>
-                    <Skeleton className="h-10 w-full"/>
-
-                </div>
-                 <div className="space-y-2">
-                    <Skeleton className="h-5 w-24"/>
-                    <Skeleton className="h-[220px] w-full"/>
-
-                </div>
-                 <div className="space-y-2">
-                    <Skeleton className="h-5 w-24"/>
-                    <Skeleton className="h-10 w-full"/>
-
-                </div>
-                
-                </div>
-                <div className="flex flex-col gap-y-8 lg:col-span-2">
-                    <div className="flex flex-col gap-4 bg-[#F9F9F9] rounded-xl overflow-hidden">
-                        <Skeleton className="aspect-video"/>
-                        <div className="px-4 py-4 space-y-6">
-                            <Skeleton  className="h-4 w-20"/>
-                            <Skeleton className="h-5 w-full"/>
-                        </div>
-                        
-                         <div className="space-y-2">
-                            <Skeleton className="h-4 w-24" />
-                            <Skeleton className="h-5 w-32" />
-                        </div>
-                        
-                         <div className="space-y-2">
-                            <Skeleton  className="h-4 w-24" />
-                            <Skeleton className="h-5 w-32" />
-                        </div>
-                        
-                        
-
-                    </div>
-
-
-                </div>
-                <div className="space-y-2">
-                    <Skeleton className="h-5 w-24"/>
-                    <Skeleton className="h-10 w-full"/>
-
-                </div>
-                
-
-            </div>
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-6">
+        <div className="space-y-2">
+          <Skeleton className="h-7 w-32" />
+          <Skeleton className="h-4 w-40" />
         </div>
-    )
-}
+        <Skeleton className="h-9 w-24" />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+        <div className="space-y-8 lg:col-span-3">
+          <div className="space-y-2">
+            <Skeleton className="h-5 w-16" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+
+          <div className="space-y-2">
+            <Skeleton className="h-5 w-24" />
+            <Skeleton className="h-[220px] w-full" />
+          </div>
+
+          <div className="space-y-2">
+            <Skeleton className="h-5 w-24" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-y-8 lg:col-span-2">
+          <div className="flex flex-col gap-4 bg-[#F9F9F9] rounded-xl overflow-hidden">
+            <Skeleton className="aspect-video" />
+
+            <div className="px-4 py-4 space-y-6">
+              <Skeleton className="h-4 w-20" />
+              <Skeleton className="h-5 w-full" />
+            </div>
+
+            <div className="px-4 space-y-4">
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-24" />
+                <Skeleton className="h-5 w-32" />
+              </div>
+
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-24" />
+                <Skeleton className="h-5 w-32" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-2 lg:col-span-5">
+          <Skeleton className="h-5 w-24" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
     const router = useRouter();
@@ -223,7 +218,7 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
                         </p>
                     </div>
                     <div className="flex items-center gap-x-2">
-                        <Button type="submit" disabled={update.isPending}>
+                        <Button type="submit" disabled={update.isPending || form.formState.isDirty}>
                             Save
                         </Button>
                         <DropdownMenu>
