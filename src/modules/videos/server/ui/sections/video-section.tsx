f9cc867b51baaf3d51389b9127d64495d1917a12
firasayari10 <ayari.firas@esprit.tn>
2025-12-02 23:58:30 +0100

    full video playing

--- src/modules/videos/server/ui/sections/video-section.tsx
+++ src/modules/videos/server/ui/sections/video-section.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 
+import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { VideoPlayer } from "../components/video-player";
 
 
 interface VideoSectionProps {
@@ -23,9 +25,17 @@ export const VideoSection = ({videoId}:VideoSectionProps)=>{
 const VideosectionSuspense = ({videoId}:VideoSectionProps)=>{
     const [video] = trpc.videos.getOne.useSuspenseQuery({id:videoId});
     return (
-        <div>
-            {JSON.stringify(video)}
+        <>
+        <div className={cn("aspect-video bg-black rounded-xl overflow-hidden relative", 
+            video.muxStatus !== "ready" && "rounded-b-none", 
+        )}>
+            <VideoPlayer 
+            autoPlay 
+            onPlay={()=>{}}
+            playbackId={video.muxPlaybackId}
+            thumbnailUrl={video.thumbnailUrl}/>
         </div>
+        </>
     )
     
 }
