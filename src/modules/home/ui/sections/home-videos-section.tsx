61257bc708ae298f9acd0ea307a836d40b7c1608
firasayari10 <ayari.firas@esprit.tn>
2026-03-13 23:00:07 +0100

    grid view for home feed

--- src/modules/home/ui/sections/home-videos-section.tsx
+++ src/modules/home/ui/sections/home-videos-section.tsx
@@ -1,5 +1,7 @@
 "use client"
+import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
+import { VideoGridCard, VideoGridCardSkeleton } from "@/modules/videos/server/ui/components/video-grid-card";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -24,9 +26,15 @@ export const HomeVideosSection=(props:HomeVideosSectionProps)=>{
 
 const HomeVideosSectionSuspenseSkeleton =()=>{
     return (
-        <p>
-            Loading 
-        </p>
+        <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5 [@media(min-width:2200px)]:grid-cols-6" >
+            {Array.from({length:18})
+            .map((_,index)=>(
+                <VideoGridCardSkeleton key={index}/>
+            ))
+            }
+        </div>
+
+        
     )
 }
 
@@ -40,7 +48,18 @@ const HomeVideosSectionSuspense = ({categoryId}:HomeVideosSectionProps)=>{
 
     return (
         <div>
-            {JSON.stringify(videos)}
+            <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5 [@media(min-width:2200px)]:grid-cols-6" >
+                {videos.pages
+                .flatMap((page)=>page.items)
+                .map((video)=>(
+                    <VideoGridCard   key={video.id} data={video}/>
+                ))
+            }
+            </div>
+        <InfiniteScroll  
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}/>
         </div>
     )
 
