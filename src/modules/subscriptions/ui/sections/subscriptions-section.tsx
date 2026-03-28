f6c18d1f0195654ab46670bbfda8585b3b612621
firasayari10 <ayari.firas@esprit.tn>
2026-03-28 23:20:51 +0100

    ui enhancements : subscriptions page

---
+++ src/modules/subscriptions/ui/sections/subscriptions-section.tsx
@@ -0,0 +1,110 @@
+"use client"
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import { DEFAULT_LIMIT } from "@/constants";
+import { VideoGridCardSkeleton } from "@/modules/videos/server/ui/components/video-grid-card";
+import {  VideoRowCardSkeleton } from "@/modules/videos/server/ui/components/video-row-card";
+import { trpc } from "@/trpc/client";
+import Link from "next/link";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { toast } from "sonner";
+import {  SubscriptionItem, SubscriptionItemSkeleton } from "../components/subscription-item";
+
+
+
+export const SubscriptionsSectionSkeleton = ()=>{
+    return (
+    <div className="flex flex-col gap-4">
+            {Array.from({length: 18}).map((_,index)=>(
+                <SubscriptionItemSkeleton  key={index} />
+            ))}
+
+        </div>)
+   
+}
+
+export const SubscriptionsSection=()=>{
+    return(
+       <Suspense fallback={<SubscriptionsSectionSuspenseSkeleton  />}>
+        <ErrorBoundary fallback={<p> error </p>}>
+        <SubscriptionsSectionSuspense  />
+
+        </ErrorBoundary>
+       </Suspense>
+    )
+}
+
+
+const SubscriptionsSectionSuspenseSkeleton = () => {
+    return (
+        <div>
+           
+            <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+                {Array.from({ length: 8 }).map((_, index) => (
+                    <VideoGridCardSkeleton key={index} />
+                ))}
+            </div>
+
+            <div className="hidden md:flex flex-col gap-4">
+                {Array.from({ length: 8 }).map((_, index) => (
+                    <VideoRowCardSkeleton key={index} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+const SubscriptionsSectionSuspense = ()=>{
+    const utils= trpc.useUtils();
+    const [subscriptions,query ]= trpc.subscriptions.getMany.useSuspenseInfiniteQuery(
+        {limit:DEFAULT_LIMIT},
+        {
+            getNextPageParam:(lastPage) =>lastPage.nextCursor,
+        }
+    );
+
+    const unsubscribe = trpc.subscriptions.remove.useMutation(
+        {
+        onSuccess:(data)=>{
+            toast.success("Unsubscribed");
+            utils.subscriptions.getMany.invalidate();
+            utils.videos.getManySubscribed.invalidate();
+            utils.users.getOne.invalidate({id: data.creatorId});
+
+           
+        },
+        onError:()=>{
+            toast.error("Something went Wrong")
+          
+        }
+    }
+    );
+
+    return (
+    <div>
+        <div className="flex flex-col gap-4 ">
+            {subscriptions.pages
+                .flatMap((page) => page.items)
+                .map((subscription) => (
+                    <Link href={`/users/${subscription.user.id}`} key={subscription.creatorId}>
+                        <SubscriptionItem
+                        name={subscription.user.name}
+                        imageUrl={subscription.user.imageUrl}
+                        subscriberCount = {subscription.user.subscriberCount}
+                        onUnsubscribe={()=>{
+                            unsubscribe.mutate({userId:subscription.creatorId})
+                        }}
+                        disabled={unsubscribe.isPending} />
+                    </Link>
+                ))}
+        </div>
+
+        <InfiniteScroll
+            hasNextPage={query.hasNextPage}
+            isFetchingNextPage={query.isFetchingNextPage}
+            fetchNextPage={query.fetchNextPage}
+        />
+    </div>
+);
+
+}
